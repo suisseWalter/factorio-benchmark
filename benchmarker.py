suisseWalter/factorio-benchmark

commit 4312f973c15b1b01c840e9eae3627b6bf1c9d08b

added the "--high_priority" argument

benchmarker.py
@@ -13,6 +13,11 @@ from zipfile import ZipFile
 
 import matplotlib.pyplot as plt
 import requests
+import psutil
+import subprocess
+import json
+import re
+
 
 outheader = [
     "timestamp",
@@ -95,6 +100,11 @@ def install_factorio(
     os.remove("factorio.zip")
 
 
+# for mypy
+def remove_character_from_string(s: str, char: str = "\r") -> str:
+    return s.replace(char, "")
+
+
 def run_benchmark(
     map_: str,
     folder: str,
@@ -103,6 +113,7 @@ def run_benchmark(
     save: bool = True,
     disable_mods: bool = True,
     factorio_bin: str | None = None,
+    high_priority: bool | None = None,
 ) -> None:
     """Run a benchmark on the given map with the specified number of ticks and
     runs."""
@@ -113,34 +124,77 @@ def run_benchmark(
         sync_mods(map_)
 
     print("Running benchmark...")
-    command = (
-        f"{factorio_bin} "
-        f'--benchmark "{map_}" '
-        f"--benchmark-ticks {ticks} "
-        f"--benchmark-runs {runs} "
-        "--benchmark-verbose all "
-        "--benchmark-sanitize"
-    )
-    # print(command)
+    # Get Version
+    factorio_log_version = os.popen(
+        f'{factorio_bin} --benchmark "{map_}" --benchmark-ticks 1 --benchmark-runs 1'
+    ).read()
+    result = re.search(r"; Factorio (\d+\.\d+\.\d+)", factorio_log_version)
+    if result:
+        version = str(
+            result[1]
+            + {
+                "linux": " Linux",
+                "win32": " Windows",
+                "cygwin": " Cygwin",
+            }[operatingsystem_codename]
+        )
 
-    factorio_log = os.popen(command).read()
+    # psutil.Popen on Linux it doesn't work well with str()
+    command: list[str] = list()
+    command.append(str(factorio_bin))
+    command.extend(["--benchmark", str(map_)])
+    command.extend(["--benchmark-ticks", str(ticks)])
+    command.extend(["--benchmark-runs", str(runs)])
+    command.extend(["--benchmark-verbose", "all"])
+    command.append("--benchmark-sanitize")
+    if high_priority is True:
+        priority = {
+            "linux": -20,
+            "win32": 128,
+            "cygwin": 128,
+        }[operatingsystem_codename]
+        print("nice = ", priority)
+        process = psutil.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process.nice(priority)
+    else:
+        process = psutil.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    factorio_log, err = map(
+        lambda a: remove_character_from_string(a.decode(), "\r"), process.communicate()
+    )
 
     if "Performed" not in factorio_log:
         print("Benchmark failed")
         print(factorio_log)
-        return
-    # print(factorio_log)
-    ups = int(
-        1000
-        * ticks
-        / float([line.split()[-2] for line in factorio_log.split("\n") if "Performed" in line][0])
-    )
-    print(f"Map benchmarked at {ups} UPS")
-    print("")
-    if save:
-        filtered_output = [line for line in factorio_log.split("\n") if "ed" in line or "t" in line]
-        with open(os.path.join(folder, "{}".format(os.path.splitext(map_)[0])), "x") as f:
-            f.write("\n".join(filtered_output))
+        print(err)
+    else:
+        if save:
+            # print(factorio_log)
+            print(version)
+            avgs = [
+                float(line.split()[-2]) / ticks
+                for line in factorio_log.split("\n")
+                if "Performed" in line
+            ]
+            avg = statistics.mean(avgs)
+            ups = 1000 / avg
+            avgs_str: list[str] = [f"{i:.3f}" for i in avgs]
+            print("Map benchmarked at:")
+            print("avg = {:.3f} ms {}".format(avg, avgs_str))
+            print("{:.3f} UPS".format(ups))
+            print()
+            out: dict[str, str | float | list[str]] = dict()
+            out["version"] = version
+            out["avg"] = avg
+            out["ups"] = ups
+            out["avgs"] = avgs_str
+            filtered_output = list()
+            filtered_output.append(str(json.dumps(out)))
+            filtered_output.extend(
+                [line for line in factorio_log.split("\n") if "ed" in line or "t" in line]
+            )
+            with open(os.path.join(folder, "{}".format(os.path.splitext(map_)[0])), "x") as f:
+                f.write("\n".join(filtered_output))
 
 
 def benchmark_folder(
@@ -153,6 +207,7 @@ def benchmark_folder(
     factorio_bin: str | None = None,
     folder: str | None = None,
     filenames: list[str] | None = None,
+    high_priority: bool | None = None,
 ) -> None:
     """Run benchmarks on all maps that match the given regular expression."""
     if not folder:
@@ -170,6 +225,7 @@ def benchmark_folder(
         save=False,
         disable_mods=disable_mods,
         factorio_bin=factorio_bin,
+        high_priority=high_priority,
     )
     print("Finished warming up, starting the actual benchmark...")
 
@@ -192,6 +248,7 @@ def benchmark_folder(
                 save=True,
                 disable_mods=disable_mods,
                 factorio_bin=factorio_bin,
+                high_priority=high_priority,
             )
 
     print("==================")
@@ -465,7 +522,6 @@ def init_parser() -> argparse.ArgumentParser:
             "forget to update afterwards.",
         ),
     )
-
     parser.add_argument(
         "-m",
         "--install_maps",
@@ -479,6 +535,13 @@ def init_parser() -> argparse.ArgumentParser:
         "--disable_mods",
         action="store_true",
         help="disables the usage of mod syncronisations. runs all benchmarks without enabling any mods",
+    )
+    parser.add_argument(
+        "-hp",
+        "--high_priority",
+        action="store_true",
+        default=False,
+        help="Increases the priority for the 'factorio' process. On Linux requires 'sudo'",
     )
     return parser
 
@@ -518,6 +581,7 @@ if __name__ == "__main__":
         args.skipticks,
         args.consistency,
         map_regex=args.regex,
+        high_priority=args.high_priority,
     )
 
     # plot_benchmark_results()

commit 60425b1c411fd4035687912e8033f220b34b8019

Feature/migration (#32)
* added factorio_version function
Before the solution involved running a full benchmark to extract the version number.
Also fixed flake8 errors introduced in 4312f97.

* Update benchmarker.py

fixed improper build detection

Co-authored-by: flameSla <39356103+flameSla@users.noreply.github.com>

* first part of migration
still highly broken, causes random factorio process to fly around.
and other weird stuff. but overall it does what it's supposed to do.

* map migration
Using psutil there are no problems with terminating factorio.

* updated readme, added custom-script
enables the running of custom scripts upon migration.

* updated requirements.txt

* converted everything to use Path objects
the benchmark_folder and migrate_folder can still take strings.
as these are potentially called from other files.
Also further code cleanup according to discussions in the PR.

* Update benchmarker.py

Co-authored-by: Ryan Delaney <1139517+rpdelaney@users.noreply.github.com>

* Update benchmarker.py

Co-authored-by: Ryan Delaney <1139517+rpdelaney@users.noreply.github.com>

Co-authored-by: flameSla <39356103+flameSla@users.noreply.github.com>
Co-authored-by: Ryan Delaney <1139517+rpdelaney@users.noreply.github.com>

benchmarker.py
@@ -1,22 +1,25 @@
 import argparse
 import atexit
 import csv
-import glob
 import itertools
 import json
 import os
 import statistics
 import subprocess
 import tarfile
+from copy import deepcopy
 from datetime import date, datetime
-from pathlib import Path
+from pathlib import Path, PurePath
 from sys import platform as operatingsystem_codename
+from typing import Any
 from zipfile import ZipFile
 
+import factorio_rcon
 import matplotlib.pyplot as plt
 import psutil
 import requests
 
+threadreg: list[subprocess.Popen[Any]] = []
 outheader = [
     "timestamp",
     "wholeUpdate",
@@ -54,14 +57,23 @@ outheader = [
 
 
 def exit_handler() -> None:
-    print("Terminating grasfully!")
-    sync_mods("", True)
+    sync_mods(PurePath(""), True)
+    for thread in threadreg:
+        thread.terminate()
+        exit_code = thread.wait()
+        print("exit_code", exit_code)
+    try:
+        os.remove(PurePath("factorio", ".lock"))
+        print("factorio was running, deleted the lock file. it might still be running.")
+    except Exception:  # noqa: PIE786
+        print("factorio was already stopped")
+        pass
     # I should also clean up potential other files
     # such as the lock file (factorio/.lock on linux)
     # also factorio.zip and maps.zip can be left over in rare cases and fail the reinstall.
 
 
-def get_factorio_version(factorio_bin: str, full: bool = False) -> str:
+def get_factorio_version(factorio_bin: PurePath, full: bool = False) -> str:
     """returns the version string of the installed factorio instance"""
     factorio_log_version = os.popen(f"{factorio_bin} --version").read()
     result = factorio_log_version.splitlines()[0].split()[1]
@@ -71,17 +83,79 @@ def get_factorio_version(factorio_bin: str, full: bool = False) -> str:
     return result
 
 
-def sync_mods(map: str, disable_all: bool = False) -> None:
+def migrate_map(
+    factorio_bin: PurePath, map: PurePath, inplace: bool, custom_script: str | None
+) -> None:
+    """migrate map to a new factorio version."""
+    print("migrating map:" + str(map))
+    if not inplace:
+        oldmap = Path(map)
+        newmap = Path(
+            PurePath.parent(map)
+            / (
+                str(PurePath.stem(map))
+                + get_factorio_version(factorio_bin).replace(".", "_")
+                + ".zip"
+            )
+        )
+        newmap.write_bytes(oldmap.read_bytes())
+        map = newmap
+    sync_mods(map)
+
+    command = f"{Path.absolute(factorio_bin)}  --start-server  {Path.absolute(map)}  --port  12245  --rcon-port  12345  --rcon-password  1234"
+    print(command)
+    proc = psutil.Popen(args=command.split("  "), stdout=subprocess.PIPE)
+    print("starting factorio server...")
+    threadreg.append(proc)
+    line: str = ""
+    lastlines = ["", "", "", "", "", "", "", ""]
+    while True:
+        lastlines.append(line)
+        lastlines.pop(0)
+        line = proc.stdout.readline().decode().rstrip()
+
+        if "Starting RCON interface at IP ADD" in line:
+            print("connecting to game...")
+            client = factorio_rcon.RCONClient("127.0.0.1", 12345, "1234")
+
+        if "New RCON connection from IP ADD" in line:
+            if custom_script is not None:
+                print("running custom script...")
+                print(custom_script)
+                resp = client.send_command(custom_script)
+                print(resp)
+            print("saving migrated map...")
+            resp = client.send_command("/server-save ")
+            print(resp)
+
+        if "Saving finished" in line:
+            print("map saved, terminating factorio...")
+            client.close()
+            proc.terminate()
+            exit_code = proc.wait()
+            print("terminated with exit code: ", exit_code)
+            print()
+            threadreg.remove(proc)
+
+            break
+        if line == "":
+            print(lastlines[1])
+            print("factorio crashed")
+            threadreg.remove(proc)
+            break
+
+
+def sync_mods(map: PurePath, disable_all: bool = False) -> None:
     fmm_name = {"linux": "fmm_linux", "win32": "fmm_win32.exe", "cygwin": "fmm_win32.exe"}[
         operatingsystem_codename
     ]
     if not disable_all:
         set_mod_command = (
-            os.path.join("fmm", fmm_name)
-            + f'  --config {os.path.join("fmm", "fmm.toml")}  sf "{map}"'
+            str(PurePath("fmm", fmm_name))
+            + f'  --config {PurePath("fmm", "fmm.toml")}  sf "{str(map)}"'
         )
     else:
-        set_mod_command = os.path.join("fmm", fmm_name) + " --game-dir factorio disable"
+        set_mod_command = f"{PurePath('fmm', fmm_name)} --game-dir factorio disable"
     # print(">>>> sync_mods()\t", set_mod_command)
     print(os.popen(set_mod_command).read())
 
@@ -114,19 +188,19 @@ def remove_character_from_string(s: str, char: str = "\r") -> str:
 
 
 def run_benchmark(
-    map_: str,
+    map_: PurePath,
     folder: str,
     ticks: int,
     runs: int,
+    factorio_bin: PurePath,
     save: bool = True,
     disable_mods: bool = True,
-    factorio_bin: str | None = None,
     high_priority: bool | None = None,
 ) -> None:
     """Run a benchmark on the given map with the specified number of ticks and
     runs."""
     if not factorio_bin:
-        factorio_bin = os.path.join("factorio", "bin", "x64", "factorio")
+        factorio_bin = PurePath("factorio", "bin", "x64", "factorio")
     # setting mods
     if not disable_mods:
         sync_mods(map_)
@@ -186,8 +260,28 @@ def run_benchmark(
             filtered_output.extend(
                 [line for line in factorio_log.split("\n") if "ed" in line or "t" in line]
             )
-            with open(os.path.join(folder, "{}".format(os.path.splitext(map_)[0])), "x") as f:
+            with open(PurePath(folder, PurePath(map_).parent, PurePath(map_).stem), "x") as f:
                 f.write("\n".join(filtered_output))
+
+
+def migrate_folder(
+    inplace: bool = False,
+    factorio_bin: str | None = None,
+    map_regex: str = "*",
+    filenames: list[Path] | list[str] | None = None,
+    custom_script: str | None = None,
+) -> None:
+    factorio_path = PurePath(
+        factorio_bin if factorio_bin else PurePath("factorio", "bin", "x64", "factorio")
+    )
+
+    files = [Path(file) for file in filenames] if filenames else [*Path("saves").glob(map_regex)]
+
+    for file in deepcopy(files):
+        if Path.is_file(file):
+            migrate_map(factorio_path, file, inplace, custom_script)
+
+    exit()
 
 
 def benchmark_folder(
@@ -199,25 +293,31 @@ def benchmark_folder(
     map_regex: str = "*",
     factorio_bin: str | None = None,
     folder: str | None = None,
-    filenames: list[str] | None = None,
+    filenames: list[str] | list[PurePath] | None = None,
     high_priority: bool | None = None,
 ) -> None:
     """Run benchmarks on all maps that match the given regular expression."""
     if not folder:
         folder = f"benchmark_on_{date.today()}_{datetime.now().strftime('%H_%M_%S')}"
-    os.makedirs(folder)
-    os.makedirs(os.path.join(folder, "saves"))
-    os.makedirs(os.path.join(folder, "graphs"))
+
+    Path(folder, "saves").mkdir(parents=True, exist_ok=True)
+    Path(folder, "graphs").mkdir(parents=True, exist_ok=True)
+
+    files = [Path(file) for file in filenames] if filenames else [*Path("saves").glob(map_regex)]
+
+    factorio_path = PurePath(
+        factorio_bin or PurePath("factorio", "bin", "x64", "factorio")
+    )
 
     print("Warming up the system...")
     run_benchmark(
-        os.path.join("saves", "factorio_maps", "big_bases", "flame10k.zip"),
+        PurePath("saves", "factorio_maps", "big_bases", "flame10k.zip"),
         folder,
         ticks=100,
         runs=1,
         save=False,
         disable_mods=disable_mods,
-        factorio_bin=factorio_bin,
+        factorio_bin=factorio_path,
         high_priority=high_priority,
     )
     print("Finished warming up, starting the actual benchmark...")
@@ -227,12 +327,11 @@ def benchmark_folder(
     print("benchmark maps")
     print("==================")
     print("")
-    if not filenames:
-        filenames = glob.glob(os.path.join("saves", map_regex), recursive=True)
-    for filename in filenames:
-        if os.path.isfile(filename):
+
+    for filename in files:
+        if filename.is_file():
             print(filename)
-            os.makedirs(os.path.join(folder, os.path.split(filename)[0]), exist_ok=True)
+            Path(folder, PurePath(filename).parent).mkdir(parents=True, exist_ok=True)
             run_benchmark(
                 filename,
                 folder,
@@ -240,7 +339,7 @@ def benchmark_folder(
                 runs=runs,
                 save=True,
                 disable_mods=disable_mods,
-                factorio_bin=factorio_bin,
+                factorio_bin=factorio_path,
                 high_priority=high_priority,
             )
 
@@ -249,43 +348,32 @@ def benchmark_folder(
     processed_table: list[list[float]] = []
     maps: list[str] = []
     errfile: list[list[int]] = []
-    old_subfolder_name = ""
-    # print(
-    #     sorted(
-    #         glob.glob(
-    #             os.path.join(
-    #                 folder,
-    #                 "saves",
-    #                 map_regex,
-    #             ),
-    #             recursive=True,
-    #         )
-    #     )
-    # )
-    for file in sorted(
-        glob.glob(
-            os.path.join(folder, "saves", map_regex),
-            recursive=True,
-        )
-    ):
+    old_subfolder = Path()
+
+    for file in sorted(Path(folder, "saves").glob(map_regex)):
         # check if file is actually a file or a folder
 
-        if not os.path.isfile(file):
+        if not Path.is_file(file):
             continue
 
         # check if we are in a new folder and if so generate the old graphs.
-        file_name = os.path.split(file)[1]
-        subfolder_name = os.path.split(os.path.split(file)[0])[1]
-        if old_subfolder_name == "":
-            old_subfolder_name = subfolder_name
+        file_name = Path(file).name
+        subfolder = Path(file).parent
+        if old_subfolder.samefile(Path()):
+            old_subfolder = subfolder
         # print(subfolder_name)
-        if subfolder_name != old_subfolder_name:
+        if not subfolder.samefile(old_subfolder):
             plot_benchmark_results(
-                processed_table, outheader, maps, folder, old_subfolder_name, errfile
+                processed_table,
+                outheader,
+                maps,
+                folder,
+                PurePath(*old_subfolder.parts[2:]),
+                errfile,
             )
             processed_table = []
             maps = []
-            old_subfolder_name = subfolder_name
+            old_subfolder = subfolder
 
         with open(file, "r", newline="") as cfile:
 
@@ -312,14 +400,16 @@ def benchmark_folder(
                 # do the consistency plot
                 plot_ups_consistency(
                     folder=folder,
-                    subfolder=old_subfolder_name,
+                    subfolder=PurePath(*old_subfolder.parts[2:]),
                     data=[a[consistency_index] for a in inlist],
                     ticks=ticks,
                     skipticks=skipticks,
                     name="consistency_" + file_name + "_" + consistency,
                 )
 
-    plot_benchmark_results(processed_table, outheader, maps, folder, old_subfolder_name, errfile)
+    plot_benchmark_results(
+        processed_table, outheader, maps, folder, PurePath(*old_subfolder.parts[2:]), errfile
+    )
 
     print("")
     print("the benchmark is finished")
@@ -328,16 +418,16 @@ def benchmark_folder(
 
 def plot_ups_consistency(
     folder: str,
-    subfolder: str,
+    subfolder: PurePath,
     data: list[float],
     ticks: int,
     skipticks: int,
     name: str = "default",
 ) -> None:
-    subfolder_path = os.path.join(folder, "graphs", subfolder)
+    subfolder_path = PurePath(folder, "graphs", subfolder)
 
-    if not os.path.exists(subfolder_path):
-        os.makedirs(subfolder_path)
+    # if not Path(subfolder_path).exists:
+    Path(subfolder_path).mkdir(parents=True, exist_ok=True)
     darray = []
     med = []
     maxi = []
@@ -367,8 +457,8 @@ def plot_ups_consistency(
     plt.ylabel(ylabel="tick time [ms]")
     plt.legend()
     plt.tight_layout()
-    # Use os.path.join to build the file path for the output image
-    out_path = os.path.join(subfolder_path, f"{name}_all.png")
+    # Use PurePath to build the file path for the output image
+    out_path = PurePath(subfolder_path, f"{name}_all.png")
     # plt.show()
     plt.savefig(out_path, dpi=800)
     plt.clf()
@@ -382,8 +472,8 @@ def plot_ups_consistency(
     plt.ylabel(ylabel="tick time [ms]")
     plt.legend()
     plt.tight_layout()
-    # Use os.path.join to build the file path for the output image
-    out_path = os.path.join(subfolder_path, f"{name}_min_max_med.png")
+    # Use PurePath to build the file path for the output image
+    out_path = PurePath(subfolder_path, f"{name}_min_max_med.png")
     # plt.show()
     plt.savefig(out_path, dpi=800)
     plt.clf()
@@ -395,14 +485,14 @@ def plot_benchmark_results(
     titles: list[str],
     maps: list[str],
     folder: str,
-    subfolder: str,
+    subfolder: PurePath,
     errfile: list[list[int]],
 ) -> None:
     """Generate plots of benchmark results."""
     # Create the output subfolder if it does not exist
-    subfolder_path = os.path.join(folder, "graphs", subfolder)
-    if not os.path.exists(subfolder_path):
-        os.makedirs(subfolder_path)
+    subfolder_path = PurePath(folder, "graphs", subfolder)
+    # if not Path(subfolder_path).exists:
+    Path(subfolder_path).mkdir(parents=True, exist_ok=True)
 
     for col in itertools.chain(range(1, 11), range(22, 32)):
         fig, ax = plt.subplots()
@@ -418,8 +508,8 @@ def plot_benchmark_results(
         ax.set_xlabel("Mean frametime [ms/frame]")
         ax.set_ylabel("Map name")
         plt.tight_layout()
-        # Use os.path.join to build the file path for the output image
-        out_path = os.path.join(subfolder_path, f"{titles[col]}.png")
+        # Use PurePath to build the file path for the output image
+        out_path = PurePath(subfolder_path, f"{titles[col]}.png")
         plt.savefig(out_path)
         plt.clf()
         plt.close()
@@ -429,15 +519,15 @@ def create_mods_dir() -> None:
     """creates a folder: 'factorio/mods'"""
     """creates a file: 'factorio/mods/mod-list.json'"""
     """copies the file from 'fmm/mod-settings.dat' to 'factorio/mods/mod-settings.dat'"""
-    os.makedirs(os.path.join("factorio", "mods"), exist_ok=True)
-    mod_list_json_file = os.path.join("factorio", "mods", "mod-list.json")
-    if not os.path.exists(mod_list_json_file):
+    os.makedirs(PurePath("factorio", "mods"), exist_ok=True)
+    mod_list_json_file = PurePath("factorio", "mods", "mod-list.json")
+    if not Path(mod_list_json_file).exists():
         with open(mod_list_json_file, "x") as file:
             file.write('{"mods":[{"name":"base","enabled":true}]}')
-    mod_settings_dat_file = os.path.join("factorio", "mods", "mod-settings.dat")
-    if not os.path.exists(mod_settings_dat_file):
+    mod_settings_dat_file = PurePath("factorio", "mods", "mod-settings.dat")
+    if not Path(mod_settings_dat_file).exists():
         # copy the file 'mod-settings.dat'
-        source: Path = Path(os.path.join("fmm", "mod-settings.dat"))
+        source: Path = Path(PurePath("fmm", "mod-settings.dat"))
         destination: Path = Path(mod_settings_dat_file)
         destination.write_bytes(source.read_bytes())
 
@@ -536,6 +626,17 @@ def init_parser() -> argparse.ArgumentParser:
         default=False,
         help="Increases the priority for the 'factorio' process. On Linux requires 'sudo'",
     )
+    parser.add_argument(
+        "-mi",
+        "--migrate",
+        nargs="?",
+        const="copy",
+        help=str(
+            "Migrates all the selected maps to the currently installed factorio version."
+            "By default it will migrate a copy of the map. To migrate in place add `inplace` as a argument"
+        ),
+    )
+    parser.add_argument("--custom_script", type=str, help="run a custom lua script upon migration.")
     return parser
 
 
@@ -546,7 +647,7 @@ if __name__ == "__main__":
     atexit.register(exit_handler)
     args = init_parser().parse_args()
     consistency_index: int = 0
-
+    create_mods_dir()
     if args.consistency is not None:
         try:
             consistency_index = outheader.index(args.consistency)
@@ -554,18 +655,34 @@ if __name__ == "__main__":
             print("the chosen consistency variable doesn't exist:", e)
             exit(0)
 
+    if args.migrate is not None:
+
+        if args.migrate == "inplace":
+            if args.custom_script:
+                migrate_folder(True, map_regex=args.regex, custom_script=args.custom_script)
+            else:
+                migrate_folder(True, map_regex=args.regex)
+        elif args.migrate == "copy":
+            if args.custom_script:
+                migrate_folder(False, map_regex=args.regex, custom_script=args.custom_script)
+            else:
+                migrate_folder(False, map_regex=args.regex)
+        exit()
+
     if args.update:
         if args.version_link:
             install_factorio(args.version_link)
         else:
             install_factorio()
+        create_mods_dir()
+        exit()
 
     if args.install_maps:
         install_maps(args.install_maps)
+        exit()
 
-    create_mods_dir()
     if args.disable_mods:
-        sync_mods(map="", disable_all=True)
+        sync_mods(map=PurePath(""), disable_all=True)
 
     benchmark_folder(
         args.ticks,